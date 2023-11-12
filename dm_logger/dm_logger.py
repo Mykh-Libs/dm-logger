44768afcaae0de85ce54a070b54be98644ddf436
Mykhailenko Dmitro <mismartconfig@gmail.com>
2023-11-13 00:31:33 +0200

    update logs record

--- dm_logger/dm_logger.py
+++ dm_logger/dm_logger.py
@@ -1,5 +1,5 @@
 from logging.handlers import RotatingFileHandler
-from typing import Callable
+from typing import Callable, Literal
 import logging
 import os.path
 import re
@@ -10,24 +10,28 @@ class DMLogger:
         self,
         name: str,
         logging_level: str = "DEBUG",
-        logs_dir: str = "logs",
+        logs_dir_path: str = "logs",
+        file_name: str = "",
+        write_mode: Literal["a", "w"] = "w",
         max_MB: int = 5,
         max_count: int = 10,
         format_string: str = "%(asctime)s.%(msecs)03d [%(levelname)s] (%(module)s.%(funcName)s:%(lineno)d) %(message)s",
-        args_separator: str = " | "
     ):
         logging_level = logging.getLevelName(logging_level.upper())
-        self._args_separator = args_separator
         self._logger = logging.getLogger(name)
         self._logger.setLevel(logging_level)
 
-        if logs_dir:
-            if not os.path.exists(logs_dir):
-                os.makedirs(logs_dir)
-            logs_path = os.path.join(logs_dir, f"{name}.log")
+        if logs_dir_path:
+            logs_dir_path = os.path.normpath(logs_dir_path)
+            if not os.path.exists(logs_dir_path):
+                os.makedirs(logs_dir_path)
+            file_name = file_name or f"{name}.log"
+            log_path = os.path.join(logs_dir_path, file_name)
+            is_exists_log = os.path.exists(log_path)
 
-            handler = RotatingFileHandler(logs_path, maxBytes=max_MB * 1024 * 1024, backupCount=max_count)
-            # handler = logging.FileHandler(logs_path, mode="w")
+            handler = RotatingFileHandler(log_path, maxBytes=max_MB * 1024 * 1024, backupCount=max_count)
+            if write_mode == "w" and is_exists_log:
+                handler.doRollover()
             formatter = logging.Formatter(format_string, datefmt='%d-%m-%Y %H:%M:%S')
 
             handler.setFormatter(formatter)
